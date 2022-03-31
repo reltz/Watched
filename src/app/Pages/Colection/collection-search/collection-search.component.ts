d337d923e95a7f7a7bbf3306d591f9919748b0f6
reltz <rpetrye@hotmail.com>
2022-03-31 11:27:35 -0400

    added reactive search on collections

--- src/app/Pages/Colection/collection-search/collection-search.component.ts
+++ src/app/Pages/Colection/collection-search/collection-search.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
-import { Observable } from 'rxjs';
+import { Observable, of, Subscription } from 'rxjs';
+import { switchMap } from 'rxjs/internal/operators/switchMap';
 import { ISearchResult } from 'src/app/Models/ApiModels';
 import { SearchCollectionsService } from 'src/app/Services/search-collections.service';
 
@@ -14,6 +15,7 @@ export class CollectionSearchComponent implements OnInit
 	public formControl: FormControl;
 	public searchResult: string[][];
 	public noResult: boolean;
+	public searchSubscription: Subscription;
 
 	constructor(
 		private svc: SearchCollectionsService,
@@ -22,16 +24,30 @@ export class CollectionSearchComponent implements OnInit
 	public ngOnInit(): void
 	{
 		this.formControl = new FormControl('', [Validators.required]);
-	}
 
-	public doSearch(): void
-	{
-		this.svc.searchCollections(this.formControl.value)
-			.subscribe((result) =>
+		this.searchSubscription = this.formControl.valueChanges.pipe(
+			switchMap(value => !!value ? this.svc.searchCollections(value as string) : of([])),
+		)
+			.subscribe(result =>
 			{
 				this.noResult = result.length < 1 ? true : false;
 				this.searchResult = result;
-			})
-			.unsubscribe();
+			});
 	}
+
+	public ngOnDestroy(): void
+	{
+		this.searchSubscription.unsubscribe();
+	}
+
+	// public doSearch(): void
+	// {
+	// 	this.svc.searchCollections(this.formControl.value)
+	// 		.subscribe((result) =>
+	// 		{
+	// 			this.noResult = result.length < 1 ? true : false;
+	// 			this.searchResult = result;
+	// 		})
+	// 		.unsubscribe();
+	// }
 }
